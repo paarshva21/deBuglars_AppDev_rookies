733e61d4a28a4249b7d094f7a3627679117a5481
Paarshva Chitaliya <paarshva111@gmail.com>
2023-02-11 18:17:30 +0530

    Changes

--- lib/Display.dart
+++ lib/Display.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Display extends StatefulWidget {
   const Display({Key? key}) : super(key: key);
@@ -8,8 +12,93 @@ class Display extends StatefulWidget {
 }
 
 class _DisplayState extends State<Display> {
+  var user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder<List<dynamic>>(
+        future: getData(user.email),
+        builder: (context, snapshot) {
+          try {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    return buildFood(snapshot.data![index]);
+                  });
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              print(snapshot.error);
+              return Center(child: (Text("Error occured!1")));
+            } else {
+              return Center(child: (Text("Error occured2!")));
+            }
+          } catch (e) {
+            print(e.toString());
+            return Center(child: (Text("Error occured!3")));
+          }
+        });
   }
+
+  Widget buildFood(Food food) {
+    return ListTile(
+      title: Text(food.name!),
+      trailing: Text(food.price!),
+      subtitle: Text(food.quantity!),
+    );
+  }
+}
+
+class Food {
+  late final int id;
+  late final String name;
+  late final String date;
+  late final String price;
+  late final String quantity;
+  late final int provider;
+
+  Food(
+      {required this.id,
+        required this.name,
+        required this.date,
+        required this.price,
+        required this.quantity,
+        required this.provider});
+
+  Food.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    date = json['date'];
+    price = json['price'];
+    quantity = json['quantity'];
+    provider = json['provider'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['date'] = this.date;
+    data['price'] = this.price;
+    data['quantity'] = this.quantity;
+    data['provider'] = this.provider;
+    return data;
+  }
+}
+
+
+Future<List<Food>> getData(String? email) async {
+  String url = "http://192.168.29.59:8000/view_food/$email";
+  http.Response response = await http.get(Uri.parse(url));
+  var data = jsonDecode(response.body);
+  print(data[0]["id"]);
+  log(data.toString());
+  headers:
+  jsonEncode(<String, String>{
+    "provider": email!,
+  });
+  return data;
 }
--- lib/Inventory.dart
+++ lib/Inventory.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
@@ -128,7 +127,7 @@ class _InventoryState extends State<Inventory> {
                 width: 135.0,
                 height: 40.0,
                 color: Colors.green,
-                child: ElevatedButton(
+                child: TextButton(
                   onPressed: () {
                     if (formKey.currentState!.validate()) {
                       foodName = _FoodName.text.trim();
@@ -157,14 +156,6 @@ class _InventoryState extends State<Inventory> {
   }
 }
 
-void getData() async {
-  String url = "http://192.168.29.59:8000/view_food/";
-  http.Response response = await http.get(Uri.parse(url));
-  var data = jsonDecode(response.body);
-  print(data[0]["id"]);
-  log(data.toString());
-}
-
 void addData(String? foodName, String? email, String? price, String? quantity) {
   print(foodName);
   print(price);
@@ -183,3 +174,4 @@ void addData(String? foodName, String? email, String? price, String? quantity) {
     }),
   );
 }
+
